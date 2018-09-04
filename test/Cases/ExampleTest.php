c255f1ec9c0429c92b6c4352f1c86a7cb635310a
xiaolin <462441355@qq.com>
2018-09-04 10:51:33 +0800

    * 单元测试修改

---
+++ test/Cases/ExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2018/9/4
+ * Time: 上午10:50
+ */
+
+namespace Swoft\Test\Cases;
+
+
+use Swoft\Test\AbstractTestCase;
+
+class ExampleTest extends AbstractTestCase
+{
+
+}
