31124e885452c379e828688031431db768adc88e
xiaolin <462441355@qq.com>
2018-09-19 16:51:29 +0800

    * Fix

--- app/Commands/QueueCommand.php
+++ app/Commands/QueueCommand.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: mac
- * Date: 2018/9/19
- * Time: 下午3:12
+ * This file is part of Swoft.
+ *
+ * @link https://swoft.org
+ * @document https://doc.swoft.org
+ * @contact group@swoft.org
+ * @license https://github.com/swoft-cloud/swoft/blob/master/LICENSE
  */
 
 namespace App\Commands;
@@ -14,19 +16,25 @@ use Swoft\Console\Input\Input;
 use Swoft\Console\Output\Output;
 
 /**
- * Class QueueCommand
+ * This is a example command class
+ * @Command(coroutine=false)
  * @package App\Commands
  */
-class QueueCommand
-{
+class QueueCommand{
     /**
-     *
-     * @Usage {command}
-     * @Example {command}
-     * @param Input  $input
+     * this is a example command
+     * @Usage {command} [arguments ...] [--options ...]
+     * @Arguments
+     *   first STRING        The first argument value
+     *   second STRING       The second argument value
+     * @Options
+     *   --opt STRING        This is a long option
+     *   -s BOOL             This is a short option(<comment>use color</comment>)
+     * @Example {command} FIRST SECOND --opt VALUE -s
+     * @param Input $input
      * @param Output $output
-     * @return int
      * @throws \Lin\Swoole\Queue\QueueException
+     * @return int
      */
     public function handle(Input $input, Output $output): int
     {
