a799a46b4d706496792da40e7fc35ddd4e576711
xiaolin <462441355@qq.com>
2018-09-20 18:02:56 +0800

    * Fix

--- app/Core/Logger/ThrowableLogger.php
+++ app/Core/Logger/ThrowableLogger.php
@@ -22,28 +22,18 @@ use Swoft\Bean\Annotation\Inject;
 class ThrowableLogger
 {
     /**
-     * @Inject("logger")
      * @var Logger
      */
     protected $logger;
 
     /**
      * 格式化 $throwable
-     * @author limx
      * @param Throwable $throwable
      * @return string
      */
     protected function format(Throwable $throwable): string
     {
-        return sprintf(
-            "%s:%s(%s) in %s:%s\nStack trace:\n%s",
-            get_class($throwable),
-            $throwable->getMessage(),
-            $throwable->getCode(),
-            $throwable->getFile(),
-            $throwable->getLine(),
-            $throwable->getTraceAsString()
-        );
+        return (string)$throwable;
     }
 
     public function __call($name, $arguments)
