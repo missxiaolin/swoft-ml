684b8580c45a56c678f318228befcc6f1c278848
xiaolin <462441355@qq.com>
2018-09-26 15:19:42 +0800

    * 增加beanScan配置

--- config/properties/app.php
+++ config/properties/app.php
@@ -29,4 +29,5 @@ return [
     'provider' => require __DIR__ . DS . 'provider.php',
     'message' => require __DIR__ . DS . 'message.php',
     'queue' => require __DIR__ . DS . 'queue.php',
+    'beanScan' => require __DIR__ . DS . 'beanScan.php',
 ];
---
+++ config/properties/beanScan.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2018/9/26
+ * Time: 下午3:19
+ */
+
+$beanScan = [
+    'App\\Breaker',
+    'App\\Controllers',
+    'App\\Core',
+    'App\\Exception',
+    'App\\Fallback',
+    'App\\Lib',
+    'App\\Listener',
+    'App\\Middlewares',
+    'App\\Models',
+    'App\\Pool',
+    'App\\Process',
+    'App\\Services',
+    'App\\Tasks',
+];
+
+return $beanScan;
