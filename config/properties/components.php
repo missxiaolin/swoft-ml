05f03aa1e85fedb293792cc6951e2a6500668243
xiaolin <462441355@qq.com>
2018-09-28 16:27:43 +0800

    * 自定义

---
+++ config/properties/components.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2018/9/28
+ * Time: 下午4:18
+ */
+
+return [
+    'custom' => [
+        'Swoftx\\Aop\\Cacheable\\',
+        'Swoftx\\Db\\Entity\\',
+        'Swoftx\\Rpc\\',
+    ],
+];
